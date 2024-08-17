d515253489f223b400f3872025dfa8f59aaad8e7
YG73 <xr.yu.life@outlook.com>
2024-08-18 02:46:37 +0800

    冒泡排序

--- src/sorting/BubbleSort.java
+++ src/sorting/BubbleSort.java
@@ -1,6 +1,7 @@
 package sorting;
 
-import java.lang.reflect.Array;
+import java.util.Date;
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 //冒泡排序
@@ -16,6 +17,12 @@ public class BubbleSort {
         boolean flag = false;//标识符：标识是否有进行交换操作，默认为false ->用来优化冒泡排序
         // ——预期效果为：当某次循环中没有交换的操作时，认为其已实现有序——则将flag设为false
         System.out.println("排序前的数组为：" + Arrays.toString(arr));
+
+//        Date date = new Date();//这个是用来计算耗时的
+//        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-mm-dd HH:mm:ss");
+//        String data1Str = simpleDateFormat.format(date);
+//        System.out.println("排序前的时间为："+data1Str);
+
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < (arr.length - 1 - i); j++) {
                 if (arr[j] > arr[j + 1]) {
@@ -31,6 +38,11 @@ public class BubbleSort {
                 flag = false;//重置flag
             }
         }
+//        Date date2 = new Date();//这个是用来计算耗时的
+//        SimpleDateFormat simpleDateFormat2 = new SimpleDateFormat("yyyy-mm-dd HH:mm:ss");
+//        String data2Str = simpleDateFormat2.format(date2);
+//        System.out.println("排序后的时间为："+data2Str);
+
         System.out.println("排序后的数组为：" + Arrays.toString(arr));
     }
 
